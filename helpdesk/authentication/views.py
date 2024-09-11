c6a9efc659c53cc4d074df6c744641030f14683e
rafinha <rbiancosilva45@gmail.com>
2024-09-11 19:08:01 -0300

    Correcting authentication routes

--- helpdesk/authentication/views.py
+++ helpdesk/authentication/views.py
@@ -6,42 +6,50 @@ from django.contrib.auth import authenticate, login
 from django.contrib import messages
 
 def index(request):
-    return render(request, "authentication/index.html")
+    if not request.user.is_authenticated:
+        return render(request, "authentication/index.html")
+    else:
+        url = reverse('index_tickets')
+        return HttpResponseRedirect(url)
 
 
 def register_view(request):
-    if request.method == 'POST':
-        # Get data from the form
-        username = request.POST.get('username', '').strip()  # using get() to avoid KeyError
-        email = request.POST.get('email', '').strip()
-        password1 = request.POST.get('password1', '').strip()
-        password2 = request.POST.get('password2', '').strip()
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            # Get data from the form
+            username = request.POST.get('username', '').strip()  # using get() to avoid KeyError
+            email = request.POST.get('email', '').strip()
+            password1 = request.POST.get('password1', '').strip()
+            password2 = request.POST.get('password2', '').strip()
 
-        # Check if all fields are filled in
-        if not username or not email or not password1 or not password2:
-            messages.error(request, "All fields are required.")
+            # Check if all fields are filled in
+            if not username or not email or not password1 or not password2:
+                messages.error(request, "All fields are required.")
+                return render(request, 'authentication/register.html')
+
+            # Perform validation (e.g., password match)
+            if password1 != password2:
+                messages.error(request, "Passwords do not match.")
+                return render(request, 'authentication/register.html')
+
+            # Check if the username already exists
+            if User.objects.filter(username=username).exists():
+                messages.error(request, "Username already taken.")
+                return render(request, 'authentication/register.html')
+
+            # Create a new user
+            user = User.objects.create_user(username=username, email=email, password=password1)
+            user.save()
+
+            # Authenticate and log the user in
+            user = authenticate(username=username, password=password1)
+            if user is not None:
+                return render(request, 'authentication/login.html')
+        else:# Redirect to home page after login
             return render(request, 'authentication/register.html')
-
-        # Perform validation (e.g., password match)
-        if password1 != password2:
-            messages.error(request, "Passwords do not match.")
-            return render(request, 'authentication/register.html')
-
-        # Check if the username already exists
-        if User.objects.filter(username=username).exists():
-            messages.error(request, "Username already taken.")
-            return render(request, 'authentication/register.html')
-
-        # Create a new user
-        user = User.objects.create_user(username=username, email=email, password=password1)
-        user.save()
-
-        # Authenticate and log the user in
-        user = authenticate(username=username, password=password1)
-        if user is not None:
-            return render(request, 'authentication/login.html')
-    else:# Redirect to home page after login
-        return render(request, 'authentication/register.html')
+    else:
+        url = reverse('index_tickets')
+        return HttpResponseRedirect(url)
 
 def login_view(request):
     if request.user.is_authenticated:
@@ -54,7 +62,7 @@ def login_view(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('index')
+                return redirect('index-tickets')
             else:
                 messages.error(request, "Username or password is incorrect.")
                 return render(request, 'authentication/login.html')
