3d862c2a3c64787d4fa62e7238603ea4064d5781
rbiancosilva <rbiancosilva45@gmail.com>
2024-09-12 10:52:10 -0300

    Ticket model created

--- helpdesk/tickets/models.py
+++ helpdesk/tickets/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Ticket(models.Model):
+    title = models.TextField(max_length=60, blank=False)
+    description = models.TextField(blank=False)
+    attachment = models.ImageField(upload_to='media/ticket_attachments', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    responsible = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="responsible_ticket")
+    opened_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="opened_ticket")
+
+    class Meta:
+        permissions = [
+            ("create_ticket", "allows to create tickets"),
+            ("delete_tickets", "allows to delete tickets"),
+            ("modify_tickets", "allows to modify tickets")
+        ]
+
+    def __str__(self):
+        return self.title
