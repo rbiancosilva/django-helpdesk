924e3b4d75b9c6043354d3aa084368e035d07441
rbiancosilva <rbiancosilva45@gmail.com>
2024-09-12 10:00:13 -0300

    Adding logout func

--- helpdesk/authentication/models.py
+++ helpdesk/authentication/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     description = models.TextField(blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pictures', blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='media/profile_pictures', blank=True, null=True)
     company = models.TextField(blank=True)
 
 
--- helpdesk/authentication/views.py
+++ helpdesk/authentication/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .models import Profile
 
@@ -74,3 +74,6 @@ def login_view(request):
         else:
             return render(request, 'authentication/login.html')
     
+def logout_view(request):
+    logout(request)
+    return render(request, "authentication/index.html")
