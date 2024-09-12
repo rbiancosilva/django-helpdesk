4a63d19a9cf1b92a9d990c6b0d9dd42301438cc1
rbiancosilva <rbiancosilva45@gmail.com>
2024-09-12 15:41:00 -0300

    Adding profile model and auth urls

--- django_helpdesk/apps/knowledge_base/apps.py
+++ django_helpdesk/apps/knowledge_base/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class KnowledgeBaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'knowledge_base'
+    name = 'django_helpdesk.apps.knowledge_base'
--- django_helpdesk/apps/user_profile/models.py
+++ django_helpdesk/apps/user_profile/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.contrib.auth.models import User 
+from django.db import models 
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, max_length=400)
+    profile_picture = models.ImageField(upload_to='media/profile_pictures', blank=True, null=True)
+    company = models.TextField(max_length=120, blank=True)
+    job = models.TextField(max_length=60, blank=True)
+    country = models.TextField(max_length=120, blank=True)
+    college = models.TextField(max_length=120, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+    
