a34c4d2c17022b3a6bf09fa28d50a1398a8222e5
rafinha <rbiancosilva45@gmail.com>
2024-09-11 21:46:37 -0300

    Adding user views base to tickets

--- helpdesk/tickets/views.py
+++ helpdesk/tickets/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from authentication.models import Profile
 
 @login_required(login_url='login')
 def index_tickets(request):
     return render(request, 'tickets/index.html')
+
+@login_required(login_url='login')
+def my_profile_tickets(request):
+    profile = Profile.objects.get(user=request.user)
+    return render(request, 'tickets/my_profile.html', {'profile':profile.user})
+
+@login_required(login_url='login')
+def profile_tickets(request, user_id:int):
+    user = User.objects.get(pk=user_id)
+    profile = Profile.objects.get(user=user)
+    return render(request, 'tickets/profile.html', {'profile':profile.user})
+
+@login_required(login_url='login')
+def list_users_tickets(request):
+    users = User.objects.all()
+    return render(request, 'tickets/list_users.html', {'users':users})
